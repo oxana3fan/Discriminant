debd103e006653077b61fd4fd2c1dab3e7a48844
Coxik <coxik.coxik@gmail.com>
2019-10-19 10:57:07 +0300

    Final

--- index.js
+++ index.js
@@ -4,29 +4,40 @@ var b = prompt("Insert b");
 var c = prompt("Insert c");
 var result = '';
 
-function showDiscriminant(a, b, c) {
+function getDiscriminant(a, b, c) {
     var d = b * b - 4 * a * c;
     return d;
 }
 
-var d = showDiscriminant(a, b, c);
+var d = getDiscriminant(a, b, c);
 result = 'Discriminant: ' + d;
 alert(result);
 
-function showQadratic(a, b, c, d) {
-    var x;
+function getQadratic(a, b, d) {
+    var x1, x2;
     var result = '';
     if (d < 0) {
         result = 'Уравнение не имеет действительных корней';
         alert(result);
-        breake;
     };
     
-   else if (d = 0) {
-        result = 
-   }
+   if (d == 0) {
+        x1 = -b / (2 * a);
+        result = 'Уравнение имеет 1 корень';
+        alert(result);
+    };
 
-    else (d < 0) {
-        result = 
-    }
+    if (d < 0) {
+        x1 = (-b - Math.sqrt(d)) / (2 * a);
+        x2 = (-b + Math.sqrt(d)) / (2 * a);
+        result = 'Уравнение имеет 2 корня';
+        alert('x1 = ' + x1);
+        alert('x2 = ' + x2);
+    };
+
+    else {
+        alert('Error');
+    };
 }
+
+getQadratic(a, b, d);
